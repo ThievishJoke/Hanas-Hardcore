630c3b3862692531104bbd0b07ef159c4b0e414b
ThievishJoke <hanayokoikimo@gmail.com>
2024-10-20 20:42:18 -0400

    Initial Commit

---
+++ src/main/java/net/hanas_hardcore/HanasHardcoreDataGenerator.java
@@ -0,0 +1,11 @@
+package net.hanas_hardcore;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class HanasHardcoreDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
